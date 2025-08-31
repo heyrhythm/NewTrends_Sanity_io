ccc63aca91b1ed1b5a916214ed6e7da825f93fd2
INDRANIL CHATTERJEE <indranil.rng2002@gmail.com>
2025-08-31 12:59:45 +0530

    update6

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizeCss: false, // disable lightningcss, fallback to postcss
+  },
   images:{
     domains:['cdn.sanity.io']
   }
